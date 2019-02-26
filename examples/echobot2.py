2dd1f3dbe1582ae58a43a7487524fa01e0d2a6ac
pagan <pagandomen@gmail.com>
2019-02-26 04:25:28 +0000

    playing around

--- examples/echobot2.py
+++ examples/echobot2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
 #
@@ -57,7 +57,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("TOKENQ", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
